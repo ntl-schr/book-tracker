00c91e38ff11d735e72ec684969872e1647adaaa
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-06-02 18:49:01 +0200

    #3: added component to test primeng and fontawesome

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-test',
+  imports: [ButtonModule],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css',
+})
+export class TestComponent {}
