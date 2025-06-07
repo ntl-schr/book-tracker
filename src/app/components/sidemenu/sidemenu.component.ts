12d0032e54abd692daac83ebc0673d14537d4a83
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-06-07 09:35:59 +0200

    #8: added first sidemenu to test routing

--- src/app/components/sidemenu/sidemenu.component.ts
+++ src/app/components/sidemenu/sidemenu.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { StyleClassModule } from 'primeng/styleclass';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-sidemenu',
-  imports: [],
+  imports: [
+    ButtonModule,
+    CommonModule,
+    HeaderComponent,
+    RouterModule,
+    StyleClassModule,
+  ],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css',
 })
+// contains the sidemenu navigation to switch between different app views
 export class SidemenuComponent {
-  // contains the sidemenu navigation to switch between different app views
+  isCollapsed = false;
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
