d397d3f638ee0cda125d5a19b35be3dc6db873eb
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-06-07 09:35:29 +0200

    #8: added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { BookListComponent } from './components/book-list/book-list.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [BookListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
